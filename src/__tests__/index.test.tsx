87bda8fe1b019e5f2b9cd7f6b73956c5f4627497
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-04-25 13:39:22 +0200

    Migrate to modern yarn, add test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 import useMedia from '../index'
 
 test('it returns a boolean', () => {
@@ -18,5 +18,34 @@ test('it returns a boolean', () => {
   render(<Foo />)
 
   expect(typeof matches).toBe('boolean')
+  window.matchMedia = originalMatchMedia
+})
+
+test('it revalidates when the media query changes', () => {
+  let originalMatchMedia = window.matchMedia
+
+  let listeners: Array<Function> = []
+
+  // @ts-ignore
+  window.matchMedia = () => ({
+    matches: false,
+    addEventListener(name, handler) {
+      listeners.push(handler)
+    },
+    removeEventListener() {},
+  })
+  let matches
+  function Foo() {
+    matches = useMedia('(screen)')
+    return null
+  }
+  render(<Foo />)
+
+  expect(typeof matches).toBe('boolean')
+  expect(matches).toBe(false)
+  act(() => {
+    listeners.forEach((listener) => listener({ matches: true }))
+  })
+  expect(matches).toBe(true)
   window.matchMedia = originalMatchMedia
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useLayoutEffect, useEffect } from 'react'
 
 let useIsomorphicEffect =
